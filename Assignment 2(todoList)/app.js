cc49c1823cb6ee0e92342b31b03e3d7594915114
Amrit Agarwal <54416489+sksketer@users.noreply.github.com>
2021-10-21 13:14:31 +0530

    update assignment2(todoList)
    almost completed

--- Assignment 2(todoList)/app.js
+++ Assignment 2(todoList)/app.js
@@ -1,5 +1,21 @@
+const newItems = document.querySelector('#addItem');
+const btn = document.querySelector('#btn');
+const list = document.querySelector('#list');
 
-function addItems() {
-    const newItem = document.getElementById('addItem');
+console.log(newItems.value);
+console.log(btn);
+console.log(list);
 
-}
+btn.addEventListener('click', function() {
+    const newItem = newItems.value;
+    if(!newItem)
+        return;
+    console.log(newItem);
+
+    const newList = document.createElement('li');
+    newList.innerText = newItem;
+    list.appendChild(newList);
+
+    newItems.value = "";
+    newItems.focus();
+})
